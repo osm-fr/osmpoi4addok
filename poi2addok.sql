793878bed9a06eee65e09c1bb7a10ff29730c6e6
cquest <cquest@openstreetmap.fr>
2017-07-25 18:06:05 +0200

    id = lien OSM, type entre () si besoin

--- poi2addok.sql
+++ poi2addok.sql
@@ -1,6 +1,6 @@
 CREATE TEMP TABLE poi_tag AS
 SELECT
-  id,
+  id::numeric,
   lat,
   lon,
   coalesce('aerialway='||aerialway, 'aerodrome='||aerodrome, 'aeroway='||aeroway, 'amenity='||amenity, 'boundary='||boundary, 'bridge='||bridge, 'craft='||craft, 'emergency='||emergency, 'heritage='||heritage, 'highway='||highway, 'historic='||historic, 'junction='||junction, 'landuse='||landuse, 'leisure='||leisure, 'man_made='||man_made, 'military='||military, 'mountain_pass='||mountain_pass, 'natural='||"natural", 'office='||office, 'place='||place, 'railway='||railway, 'shop='||shop, 'tourism='||tourism, 'tunnel='||tunnel, 'waterway='||waterway) AS tag,
@@ -23,10 +23,14 @@ SELECT
   row_to_json(t)
 FROM (
   SELECT
-    id,
+    format('http://osm.org/%s/%s',case when id<1000000000000000 then 'node' when id>2000000000000000 then 'relation' else 'way' end, id % 1000000000000000) as id,
     'poi' AS type,
     value1 AS poi,
-    (SELECT array_agg(case when name like '%'||trim(v)||'%' then name else trim(v) || coalesce(' ' || name, '') end) FROM unnest(regexp_split_to_array(label, ' / ')) AS t(v)) AS name,
+    (SELECT array_agg(case
+      when lower(unaccent(name)) like '%'||lower(unaccent(trim(v)))||'%' then name
+      when name is null then trim(v)
+      else trim(format('%s (%s)',coalesce(name,''),trim(v))) end)
+      FROM unnest(regexp_split_to_array(label, ' / ')) AS t(v)) AS name,
     lat,
     lon,
     city,
