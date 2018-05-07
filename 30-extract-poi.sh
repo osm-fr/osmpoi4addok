c8030a67cb4ecc84f4ea50162439fd4eb47325a1
cquest <cquest@openstreetmap.fr>
2018-05-07 18:19:02 +0200

    ajout bbox objet geo et noms alternatifs

--- 30-extract-poi.sh
+++ 30-extract-poi.sh
@@ -4,7 +4,7 @@ PBF=http://download.geofabrik.de/europe/france-latest.osm.pbf
 FILTER="`tail -n +2 def.csv | cut -d ',' -f 1,2,3 | sort | sed -e 's/\([-_a-z0-9]\+\),\([-_a-z0-9]\+\),\([-_a-z0-9]*\)/( \1=\2 and \3= ) or/' | tr '\n' ' ' | sed -e 's/ and =//g' | sed -e 's/ or \$//'`"
 osmconvert france-latest.osm.pbf -o=france-latest.o5m
 osmfilter france-latest.o5m --keep="$FILTER" -o=france-poi.o5m
-osmconvert france-poi.o5m --all-to-nodes --max-objects=1000000000 -o=france-poi-all-to-nodes.o5m
-osmfilter france-poi-all-to-nodes.o5m --keep="$FILTER" -o=france-poi.o5m
+osmconvert france-poi.o5m --all-to-nodes --add-bbox-tags --max-objects=1000000000 -o=france-poi-all-to-nodes.o5m
+osmfilter france-poi-all-to-nodes.o5m --keep="$FILTER" -o=france-poi-bbox.o5m
 KEY="`tail -n +2 def.csv | cut -d ',' -f 1 | sort -u | tr '\n' ' '`"
-osmconvert france-poi.o5m --csv="@id @lon @lat name $KEY" --csv-headline > poi.csv
+osmconvert france-poi-bbox.o5m --csv="@id @lon @lat bBox name alt_name short_name name:fr local_name official_name old_name $KEY" --csv-headline > poi.csv
