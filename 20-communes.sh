6f3eed977bf1dfbaa04b796548964b7fb551297b
cquest <cquest@openstreetmap.fr>
2018-05-07 18:17:39 +0200

    2017 > 2018

--- 20-communes.sh
+++ 20-communes.sh
@@ -1,8 +1,8 @@
-COMMUNES=communes-20170112
+COMMUNES=communes-20180101
 URL=http://osm13.openstreetmap.fr/~cquest/openfla/export/$COMMUNES-shp.zip
 [ -f $COMMUNES-shp.zip ] || wget $URL && unzip $COMMUNES-shp.zip
 psql -c "DROP TABLE IF EXISTS com;"
-shp2pgsql -d $COMMUNES.shp | psql -q
+shp2pgsql -d $COMMUNES*.shp | psql -q
 psql -c "ALTER TABLE \"$COMMUNES\" RENAME TO com;"
 psql -c "CREATE INDEX idx_com_geom ON com USING gist(geom);"
 rm $COMMUNES.* LICENCE.txt communes-descriptif.txt
