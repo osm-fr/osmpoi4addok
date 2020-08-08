ecab858103a3351a95d9af6f138140c6085847e2
cquest <cquest@openstreetmap.fr>
2020-08-08 11:07:43 +0200

    substitution des \ en /

--- 40-poi2addok.sh
+++ 40-poi2addok.sh
@@ -1,5 +1,5 @@
 psql < poi.sql
-tail -n +2 poi.csv | psql -c "COPY poi FROM STDIN WITH NULL ''"
+tail -n +2 poi.csv | sed 's!\\!/!g' | psql -c "COPY poi FROM STDIN WITH NULL ''"
 
 psql -c "SELECT AddGeometryColumn ('public','poi','geom',0,'POINT',2);"
 psql -c "UPDATE poi SET geom = ST_MakePoint(lon, lat);"
